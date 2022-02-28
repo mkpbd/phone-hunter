52844dd5c3f06019dd3121aacb9e4d00c03c725b
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-28 22:04:36 +0600

    show  iphone in html page

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ const loadMoblieApi =  async()=>{
         const apiUrl = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
         const apiUrlData = await apiUrl.json();
 
-        console.log(apiUrlData);
+        // console.log(apiUrlData);
+          await displayMobileData(apiUrlData);
 
     }catch(error){
         console.log(error);
@@ -16,3 +17,45 @@ const loadMoblieApi =  async()=>{
 }
 
 loadMoblieApi();
+
+
+const displayMobileData = async (datas)=>{
+   
+    // parent Id 
+    const showAllMobileByParentGridId = document.getElementById('show-all-phone');
+
+    // get only mobile data from api 
+    const mobileDataArray = datas.data;
+
+    mobileDataArray.forEach(data => {
+    
+     // create child elements div
+    const div = document.createElement('div');
+    div.setAttribute('class', 'col');
+
+        div.innerHTML = `
+   
+        <div class="card shadow p-3 mb-5 bg-body rounded border-0">
+          <img src="${data.image}" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${data.phone_name}</h5>
+            <h5 class="card-title">${data.brand}</h5>
+          </div>
+        </div>
+    
+        `
+
+      //  console.log(div)
+        showAllMobileByParentGridId.appendChild(div);
+    });
+ 
+
+    
+    
+
+
+ 
+
+    
+    console.log(mobileDataArray);
+}
