005932ce53e7e7bfc6e8d7e8e861ecf06dc93db7
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-28 21:46:51 +0600

    api Url Load data console log show data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("phone gunner")
+// console.log("phone gunner")
+
+/*****================ Load moblie product by search ===================*** */
+
+const loadMoblieApi =  async()=>{
+
+    try{
+        const apiUrl = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+        const apiUrlData = await apiUrl.json();
+
+        console.log(apiUrlData);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+loadMoblieApi();
