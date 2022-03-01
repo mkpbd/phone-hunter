eb0db8db3664921db6727f35abc03271fa3a58f3
mkpbd <mostofakamal1990@gmail.com>
2022-03-01 14:58:04 +0600

    api show details style Update

--- js/app.js
+++ js/app.js
@@ -101,43 +101,47 @@ const displayMobileData = async (datas)=>{
                 <h6 class="card-text title-font-face release-font-size">${details.releaseDate != '' ? details.releaseDate: 'Release Date not found'}</h6>
                 
                 <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-6">
                             <span class="icon icon-size"><i class="fa-solid fa-mobile-screen-button"></i></span>
                             <div>${details.mainFeatures.displaySize}</div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-6">
                             <span class="icon icon-size"><i class="fa-solid fa-camera"></i></span>
                             <div> ${details.mainFeatures.chipSet}   </div>
 
 
                         </div>
-                        <div class="col-md-4">
-                            <span class="icon icon-size"><i class="fa-solid fa-battery-full"></i></span>
-                        </div>
-                        <div class="col-md-4">
+
+                        <div class="col-md-6">
+                        <span class="icon icon-size"><i class="fa-solid fa-memory"></i></span>
+                        <div> ${details.mainFeatures.memory} </div>
+                    </div>
+                  
+                    <div class="col-md-6">
+                        <span class="icon icon-size"><i class="fa-solid fa-hard-drive"></i></span>
+                        <div> ${details.mainFeatures.storage} </div>
+                    </div>
+                        <div class="col-md-12">
                             <img src="./images/sensor.png"/> <br>
-                            <ul class="list-group" id="sensor-data">
+                            <ul class="list-group d-flex" id="sensor-data">
                            <!-- <li class="list-group-item"> ${ details.mainFeatures.sensors.join(" ")}</li> -->
                                
                             </ul>
                          
                           
                         </div>
-                        <div class="col-md-4">
-                            <span class="icon icon-size"><i class="fa-solid fa-memory"></i></span>
-                            <div> ${details.mainFeatures.memory} </div>
-                        </div>
-                      
-                        <div class="col-md-4">
-                            <span class="icon icon-size"><i class="fa-solid fa-hard-drive"></i></span>
-                            <div> ${details.mainFeatures.storage} </div>
-                        </div>
+                    
                       
                 </div>
            
             </div>
           </div>
+          <!-- ========== others information =============== -->
+          <div class="com-md-10 col-sm-12">
+              <h2>other information </h2>
+          </div>
         </div>
+
       </div>
         
         `;
@@ -169,7 +173,7 @@ const displayMobileData = async (datas)=>{
           // class  attribute 
           li.setAttribute('class', 'list-group-item');
 
-          li.innerText = x;
+          li.innerHTML = `<i class="fa-solid fa-check me-2"></i> ${x}` ;
 
           ulSensor.appendChild(li);
 
